9a2d09e28de807be7602c4ae3f156834b8c5adfa
Bernhard Schussek <bschussek@gmail.com>
2015-08-14 22:41:37 +0200

    Used $ignoreCase switch when checking for ".bat" extensions

--- src/PuliRunner.php
+++ src/PuliRunner.php
@@ -60,7 +60,7 @@ class PuliRunner
             }
         }
 
-        if (Path::hasExtension(strtolower($puli), '.bat')) {
+        if (Path::hasExtension($puli, '.bat', true)) {
             $this->puli = $puli;
         } else {
             $this->puli = $php.' '.$puli;
